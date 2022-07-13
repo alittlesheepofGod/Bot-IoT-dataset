f3e9413bd2cf0f51e0d4f12a2b4e987d977da1cb
alittlesheepofGod <huynhminhchau.k14@gmail.com>
2022-07-13 16:22:28 +0900

    ...

---
+++ cnn_model_base_3_different_attacks.py
@@ -0,0 +1,224 @@
+# import libraries
+from turtle import clear
+from keras.models import Sequential
+from keras.callbacks import CSVLogger, ModelCheckpoint
+from keras.layers import Conv2D, Conv1D, MaxPooling2D, MaxPooling1D, Flatten, BatchNormalization, Dense
+import plotly.express as px
+import plotly.offline as pyo
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neural_network import MLPClassifier
+import keras
+import os, re, time, math, tqdm, itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.optimizers import Adagrad
+from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+# import module handling
+import handling 
+
+X_train = handling.X_train
+X_test = handling.X_test
+
+y_train = handling.y_train
+y_test = handling.y_test
+
+# resize 
+X_train = np.resize(X_train, (X_train.shape[0], X_train.shape[1]))
+X_test = np.resize(X_test, (X_test.shape[0], X_train.shape[1]))
+
+# normalizer
+scaler = Normalizer().fit(X_train)
+X_train = scaler.transform(X_train)
+scaler = Normalizer().fit(X_test)
+X_test = scaler.transform(X_test)
+
+# standard scaler 
+s = StandardScaler()
+s.fit(X_train)
+X_train = s.transform(X_train)
+X_test = s.transform(X_test)
+
+# increase features for cnn to 72 features:
+X_train = np.resize(X_train, (X_train.shape[0], 72))
+X_test = np.resize(X_test, (X_test.shape[0], 72))
+
+#newcode pca train 
+pca=PCA(n_components=72)
+pca.fit(X_train)
+x_train_pca=pca.transform(X_train)
+#newcode pca test
+pca.fit(X_test)
+x_test_pca=pca.transform(X_test) 
+
+# increase features for cnn to 72 features:
+X_train = np.resize(X_train, (X_train.shape[0], 72, 1))
+X_test = np.resize(X_test, (X_test.shape[0], 72, 1))
+
+# CNN model
+def model():
+    model = Sequential()
+    model.add(Conv1D(filters=64, kernel_size=3, activation='relu', padding='same', input_shape=(72, 1)))
+    model.add(Conv1D(filters=128, kernel_size=1, activation='relu'))
+    # adding a pooling layer
+    model.add(MaxPooling1D(pool_size=(3), strides=1, padding='same'))
+    model.add(Conv1D(filters=128, kernel_size=1, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=(3), strides=1, padding='same'))
+    model.add(Conv1D(filters=128, kernel_size=1, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=(3), strides=1, padding='same'))
+    model.add(Conv1D(filters=128, kernel_size=1, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=(3), strides=1, padding='same'))
+    model.add(Conv1D(filters=128, kernel_size=1, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=(3), strides=1, padding='same'))
+    model.add(Conv1D(filters=128, kernel_size=1, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=(3), strides=1, padding='same'))
+    model.add(Conv1D(filters=128, kernel_size=1, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=(3), strides=1, padding='same'))
+    model.add(Conv1D(filters=128, kernel_size=1, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=(3), strides=1, padding='same'))
+
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dense(128, activation='relu'))
+    model.add(BatchNormalization())
+    # model.add(Dense(4, activation='softmax'))  # number of node in dense layer represent for number of classes to classification
+    model.add(Dense(2, activation='sigmoid'))  # number of node in dense layer represent for number of classes to classification
+    # model.add(Dense(11, activation='softmax'))  # number of node in dense layer represent for number of classes to classification
+
+    opt = SGD(lr = 0.01, momentum = 0.9, decay = 0.01)
+    # opt = Adagrad()
+
+    model.compile(loss='binary_crossentropy', optimizer = opt, metrics=['accuracy'])
+    return model
+    # adding a pooling layer 
+
+model = model()
+model.summary()
+
+history = model.fit(X_train, y_train, epochs=50, batch_size=64, validation_data=(X_test, y_test), verbose = 1)
+
+# Visualization of Results (CNN)
+# Let's make a graphical visualization of results obtained by applying CNN to our data 
+scores = model.evaluate(X_test, y_test, verbose = 1)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+
+# epochs = range(1, len(history['loss']) + 1)
+# acc = history['accuracy']
+# loss = history['loss']
+# val_acc = history['val_accuracy']
+# val_loss = history['val_loss']
+
+# draw configure matrix 
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+y_pred = model.predict(X_test)
+# labels = ["Benign", "DoS attacks-GoldenEye", "DoS attacks-Slowloris"]
+# labels = ["Benign", "DoS attacks-SlowHTTPTest", "DoS attacks-Hulk"]
+# labels = ["Benign", "DDOS attacks-LOIC-UDP", "DDOS attack-HOIC"]
+# labels = ["Benign", "Brute Force -Web", "Brute Force -XSS", "SQL Injection"]
+# labels = ["Benign", "Bot"]
+# labels = ["Benign", "Infilteration"]
+
+# # convert to categorical 
+# from keras.utils.np_utils import to_categorical
+# y_predict = to_categorical(np.argmax(y_pred, 1), dtype="int64")
+# # convert one-hot encoding to integer
+# y_predict = np.argmax(y_predict, axis=1)
+# y_test = np.argmax(y_test, axis=1)
+# cm = confusion_matrix(y_test, y_predict)
+# disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+# disp.plot(cmap=plt.cm.Blues)
+# plt.show()
+
+# # visualize training and val accuracy
+# # plt.figure(figsize=(10, 5))
+# # plt.title('Training and Validation Loss (CNN)')
+# # plt.xlabel('Epochs')
+# # plt.ylabel('Loss')
+# # plt.plot(epochs, loss, label='loss', color='g')
+# # plt.plot(epochs, val_loss, label='val_loss', color='r')
+# # plt.legend()
+
+# # list all data in history
+# print(history.history.keys())
+# # summarize history for accuracy
+# plt.plot(history.history['accuracy'])
+# plt.plot(history.history['val_accuracy'])
+# plt.title('model accuracy')
+# plt.ylabel('accuracy')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.show()
+
+# # summarize history for loss
+# plt.plot(history.history['loss'])
+# plt.plot(history.history['val_loss'])
+# plt.title('model loss')
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.show()
+
+# # print out accuracy for each class
+# matrix = confusion_matrix(y_test, y_predict)
+# # print("accuracy of benign, DoS attacks-SlowHTTPTest, DoS attacks-Hulk")
+# # print("accuracy of benign, DoS attacks-LOIC-HTTP")
+# # print(" accuracy of Benign, Brute Force -Web, Brute Force -XSS, SQL Injection: ") 
+# print(" accuracy of Benign, Infilteration: ") 
+# print(matrix.diagonal()/matrix.sum(axis=1))
+
+# # print out False Alarm Rate 
+# print("False Alarm Rate is : ")
+# FAR = 0
+# for i in range(1, len(cm[0])):
+#     FAR += cm[0][i]
+# FAR = FAR / (cm[0][0] + FAR)
+# print(FAR)
+
+# # print detection rate
+# print("Detection Rate is : ")
+# DTrate = 0
+# for i in range(1, len(cm)):
+#     for j in range(0, len(cm[i])):
+#         DTrate += cm[i][j]
+
+# sum = 0
+# for i in range(1, len(cm)):
+#     sum += cm[i][i]
+
+# DTrate = sum / DTrate 
+
+# print(DTrate)
+
+# # Conclusion after CNN Training 
+
+# """
+# After training our deep CNN model on training data and validating it on validation data, it can be 
+# interpreted that:
+
+# + Model was trained on 10 epochs
+# + CNN performed exceptionally well on training data and the accuracy was 99%
+# + Model accuracy was down to 83.55% on validation data after 50 iterations, and gave a good accuracy
+# of 92% after 30 iterations. Thus, it can be interpreted that optimal number of iterations on which this
+# model can perform are 30. --> ... 
+
+
+# """
+
---
+++ handling.py
@@ -0,0 +1,229 @@
+# import libraries 
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+pd.set_option('display.max_columns', None)
+import matplotlib.pyplot as plt
+import seaborn as sns
+from keras.utils.np_utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+
+# path to dataset 
+# PATH_TO_DATASET = "/mnt/d/project/dataset/cse-cic-ids2018/02-14-2018.csv/02-14-2018.csv"
+# PATH_TO_DATASET = "/mnt/d/project/dataset/cse-cic-ids2018/02-15-2018.csv/02-15-2018.csv"
+# PATH_TO_DATASET = "/mnt/d/project/dataset/cse-cic-ids2018/02-16-2018.csv/02-16-2018.csv"
+
+""" because 02-20-2018.csv is too large, it is divided into 8 files  """
+
+# PATH_TO_DATASET = "/mnt/d/project/dataset/cse-cic-ids2018/02-20-2018.csv/02-20-2018-01.csv"
+# PATH_TO_DATASET = "/mnt/d/project/dataset/cse-cic-ids2018/02-21-2018.csv/02-21-2018.csv"
+# PATH_TO_DATASET = "/mnt/d/project/dataset/cse-cic-ids2018/02-22-2018.csv/02-22-2018.csv"
+# PATH_TO_DATASET = "/mnt/d/project/dataset/cse-cic-ids2018/02-28-2018.csv/02-28-2018.csv"
+# PATH_TO_DATASET = "/mnt/d/project/dataset/cse-cic-ids2018/03-01-2018.csv/03-01-2018.csv"
+# PATH_TO_DATASET = "/mnt/d/project/dataset/cse-cic-ids2018/03-02-2018.csv/03-02-2018.csv"
+# PATH_TO_DATASET = "/mnt/d/project/dataset/cse-cic-ids2018/02-28-2018.csv/02-28-2018.csv"
+# PATH_TO_DATASET = "/mnt/d/project/dataset/cse-cic-ids2018/03-01-2018.csv/03-01-2018.csv"
+# PATH_TO_DATASET = "/mnt/d/project/dataset/Bot-Iot/Bot-Iot/BoT-IoT Dataset/dataset.csv"
+
+PATH_TO_DATASET = "/mnt/d/project/dataset/Bot-Iot/Bot-Iot/BoT-IoT Dataset/DDoS/DDoS_HTTP/DDoS_HTTP[1].csv"
+
+
+# open dataset csv file by 'pandas'
+dataset = pd.read_csv(PATH_TO_DATASET)
+
+# get an overview of the data 
+dataset.head()
+dataset.sample(10)
+dataset.tail()
+
+# shape of dataset
+print("shape of dataset is : ", dataset.shape)
+
+# # select only rows with label = 'Benign'
+# benign = dataset.loc[dataset['Label'] == 'Benign']
+
+# # select only rows from 1 to 50000
+# benign = benign[1:70000]
+
+# # select only rows with label = 'Infilteration'
+# infilteration = dataset.loc[dataset['Label'] == 'Infilteration']
+
+# # concat two dataframe to form dataset 
+# dataset = pd.concat([benign, infilteration], axis=0)
+
+# identify variables 
+dataset.dtypes 
+print(dataset.info(verbose=True, max_cols=True, null_counts=True))
+
+# remove object data type in dataset 
+# remove Flow ID, Src IP, Dst IP
+# dataset = dataset.drop(['Flow ID', 'Src IP', 'Dst IP', 'Src Port'], axis = 1)
+
+# print dataset after drop columns with object data type and Src Port column due to many missing values
+print(dataset.info(verbose=True, max_cols=True, null_counts=True))
+
+# # get count of missing values in the dataset 
+# print(dataset.isnull().sum())
+
+# # print out columns of dataset/dataframe 
+# print(dataset.columns)
+
+# # describe predictors variables 
+# print(dataset.describe())
+
+# # draw scatter plot 
+# dataset.plot.scatter(x = 'Flow Duration', y = 'Tot Fwd Pkts', c = 'Label', colormap='viridis')
+
+# # visualize patterns in the data 
+# dataset.corr()
+
+# check the shape of the dataset dataframe
+print("shape of dataset : ", dataset.shape)
+print("\n")
+
+# check the number of values for labels 
+print("the number of values for labels : ", dataset['Sub_Cat'].value_counts())
+
+# Data Visualizations 
+
+# After getting some useful information about our data, we now make visuals of our data to see how 
+# the trend in our data goes like. The visuals include bar plots, distribution plots, scatter plots, etc. 
+sns.set(rc={'figure.figsize':(12, 6)})
+plt.xlabel('Attack Type')
+sns.set_theme()
+ax = sns.countplot(x='Sub_Cat', data = dataset)
+ax.set(xlabel='Attack Type', ylabel='Number of Attack and Benign')
+plt.show()
+
+# shaping the data for CNN
+
+"""
+For applying a convolutional neural network on our data, we will have to follow following steps:
+
+- separate the data of each of the labels
+- create a numerical matrix representation of labels
+- apply resampling on data so that can make the distribution equal for all labels 
+- create X (predictor) and Y (target) variables 
+- split the data into train and test sets 
+- make data multi-dimensional for CNN
+- apply CNN on data --> on separated branch
+
+"""
+
+# encode the column labels
+label_encoder = LabelEncoder()
+dataset['Sub_Cat']= label_encoder.fit_transform(dataset['Sub_Cat'])
+dataset['Sub_Cat'].unique()
+
+# make 3 separate datasets for 3 feature labels 
+data_0 = dataset[dataset['Sub_Cat'] == 0]
+data_1 = dataset[dataset['Sub_Cat'] == 1]
+
+# make benign feature 
+y_0 = np.zeros(data_0.shape[0])
+y_benign = pd.DataFrame(y_0)
+
+# make attack feature 
+y_1 = np.ones(data_1.shape[0])
+y_attack_1 = pd.DataFrame(y_1)
+
+# merging the original dataframe 
+# X = pd.concat([data_0, data_1, data_2], sort = True)
+X = pd.concat([data_0, data_1], sort = True)
+# X = pd.concat([data_0, data_1, data_2, data_3], sort = True)
+# X = pd.concat([data_0, data_1, data_2, data_3, data_4, data_5, data_6, data_7, data_8, data_9, data_10], sort = True)
+
+y = pd.concat([y_benign, y_attack_1], sort = True)
+# y = pd.concat([y_benign, y_attack_1, y_attack_2, y_attack_3, y_attack_4, y_attack_5, y_attack_6, y_attack_7, y_attack_8,\
+    #  y_attack_9, y_attack_10], sort = True)
+# y = pd.concat([y_benign, y_attack_1, y_attack_2, y_attack_3], sort = True)
+
+# Data augmentation 
+
+from sklearn.utils import resample 
+
+# data_0_resample = resample(data_0, n_samples = 20000, random_state = 123, replace = True)
+# data_1_resample = resample(data_1, n_samples = 20000, random_state = 123, replace = True)
+data_0_resample = resample(data_0, n_samples = 20000, random_state = 123, replace = True)
+data_1_resample = resample(data_1, n_samples = 20000, random_state = 123, replace = True)
+# data_2_resample = resample(data_2, n_samples = 20000, random_state = 123, replace = True)
+# data_3_resample = resample(data_3, n_samples = 20000, random_state = 123, replace = True)
+
+# train_dataset = pd.concat([data_0_resample, data_1_resample, data_2_resample])
+# train_dataset = pd.concat([data_0_resample, data_1_resample, data_2_resample, data_3_resample])
+train_dataset = pd.concat([data_0_resample, data_1_resample])
+# train_dataset = pd.concat([data_0_resample, data_1_resample])
+# train_dataset = pd.concat([data_0_resample, data_1_resample, data_2_resample, data_3_resample, data_4_resample, \
+#     data_5_resample, data_6_resample, data_7_resample, data_8_resample, data_9_resample, data_10_resample])
+train_dataset.head(2)
+
+# viewing the distribution of intrusion attacks in our dataset 
+plt.figure(figsize = (10, 8))
+circle = plt.Circle((0, 0), 0.7, color = 'white')
+plt.title('Intrusion Attack Type Distribution')
+# plt.pie(train_dataset['Label'].value_counts(), labels = ['Benign', 'FTP-BruteForce', 'SSH-Bruteforce'], colors = ['blue', 'green', 'yellow'])
+# plt.pie(train_dataset['Label'].value_counts(), labels = ['Benign', 'DoS attacks-GoldenEye', 'DoS attacks-Slowloris'], colors = ['blue', 'green', 'yellow'])
+# plt.pie(train_dataset['Label'].value_counts(), labels = ['Benign', 'DoS attacks-SlowHTTPTest', 'DoS attacks-Hulk'], colors = ['blue', 'green', 'yellow'])
+# plt.pie(train_dataset['Label'].value_counts(), labels = ['Benign', 'DoS attacks-SlowHTTPTest', 'DoS attacks-Hulk'], colors = ['blue', 'green', 'yellow'])
+# plt.pie(train_dataset['Label'].value_counts(), labels = ['Benign', 'Brute Force -Web', 'Brute Force -XSS', 'SQL Injection'], colors = ['blue', 'green', 'yellow', 'pink'])
+plt.pie(train_dataset['Sub_Cat'].value_counts(), labels = ['DDoS_HTTP', 'Normal'], colors = ['black', 'white'])
+
+p = plt.gcf()
+p.gca().add_artist(circle)
+
+# making X & Y Variables (CNN)
+test_dataset = train_dataset.sample(frac=0.2)
+target_train = train_dataset['Sub_Cat']
+target_test = test_dataset['Sub_Cat']
+target_train.unique(), target_test.unique()
+
+# y_train = to_categorical(target_train, num_classes = 4)
+# y_test = to_categorical(target_test, num_classes = 4)
+
+y_train = to_categorical(target_train, num_classes = 2)
+y_test = to_categorical(target_test, num_classes = 2)
+
+# Data Splicing 
+# data into train & test sets. training data used for training model, test data used 
+# to check the performance of model on unseen dataset. 
+# 80% for training and 20% for testing purpose.
+train_dataset = train_dataset.drop(columns = ["Timestamp", "Protocol", "Label", "Cat", "Flow_ID", "Src_IP", "Dst_IP"], axis = 1)
+test_dataset = test_dataset.drop(columns = ["Timestamp", "Protocol", "Label", "Cat", "Flow_ID", "Src_IP", "Dst_IP"], axis = 1)
+
+# train_dataset = train_dataset.drop(columns = ["Flow ID", "Dst IP", "Src IP" ,"Src Port", "Timestamp", "Protocol","PSH Flag Cnt","Init Fwd Win Byts","Flow Byts/s","Flow Pkts/s", "Label"], axis = 1)
+# test_dataset = test_dataset.drop(columns = ["Flow ID" , "Dst IP", "Src IP" ,"Src Port", "Timestamp", "Protocol","PSH Flag Cnt","Init Fwd Win Byts","Flow Byts/s","Flow Pkts/s", "Label"], axis = 1)
+
+# making train & test splits 
+X_train = train_dataset.iloc[:, : -1].values 
+X_test = test_dataset.iloc[:, : -1].values 
+
+# X_train = train_dataset.iloc[:, : -1] 
+# X_test = test_dataset.iloc[:, : -1]
+
+# reshape the data for CNN 
+X_train = X_train.reshape(len(X_train), X_train.shape[1], 1)
+X_test = X_test.reshape(len(X_test), X_test.shape[1], 1)
+
+# # view data
+# display(X_train)
+
+
+# import matplotlib.pyplot as plt 
+
+# X_train.plot(kind= 'bar')
+
+# # copy the data 
+# df_max_scaled = X_train.copy()
+
+# # apply normalization techniques 
+# for column in df_max_scaled.columns:
+#     df_max_scaled[column] = df_max_scaled[column] / df_max_scaled[column].abs().max()
+
+# # view normalized data 
+# display(df_max_scaled)
+
+# import matplotlib.pyplot as plt 
+# df_max_scaled.plot(kind='bar')
+
+
+
+
